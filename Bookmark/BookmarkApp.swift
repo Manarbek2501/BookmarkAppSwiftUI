f2e0ac95a236b67b74d6f9f479d7090b584bfd2e
Manarbek.2501 <b.manarbek@icloud.com>
2023-04-18 16:39:59 +0600

    Initial Commit

---
+++ Bookmark/BookmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookmarkApp.swift
+//  Bookmark
+//
+//  Created by Manarbek Bibit on 18.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
